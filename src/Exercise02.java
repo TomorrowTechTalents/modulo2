1e076324104a691aded3e8b521c258ae4221e47c
marcmam2 <marcmam2@gmail.com>
2022-05-07 16:39:52 -0300

    add options menu

--- src/Exercise02.java
+++ src/Exercise02.java
@@ -4,26 +4,67 @@ import java.util.ArrayList;
 
 public class Exercise02 {
     public static void main(String[] args) {
-        Stock stock = new Stock();
+        Scanner scanner = new Scanner(System.in);
 
-        Product product1 = new Product();
-        stock.addProduct(product1);
+        List<Stock> stocks = new ArrayList<>();
 
-        Product product2 = new Product();
-        stock.addProduct(product2);
+        System.out.println("cadastre um estoque:");
+        System.out.print("nome: ");
+        String name = scanner.nextLine();
+        Stock stock = new Stock(name);
+        stocks.add(stock);
 
-        Product product3 = new Product();
-        stock.addProduct(product3);
 
-        stock.listProducts();
+        while (true) {
+            System.out.println("escolha uma opção:");
+            System.out.println("1 - cadastrar novo estoque");
+            System.out.println("2 - cadastrar novo produto");
+            System.out.println("3 - ver detalhes de um produto");
+            System.out.println("4 - editar dados de um produto");
+            System.out.println("5 - remover produto");
+            System.out.println("6 - listar produtos");
+            System.out.println("7 - sair");
 
-        Product product = stock.findProductByIdentificationNumber(2);
+            int option = scanner.nextInt();
+            scanner.nextLine();
 
-        stock.deleteProduct(1);
-
-        stock.listProducts();
-
-//        System.out.println(product2);
+            switch (option) {
+                case 1:
+                    System.out.print("nome: ");
+                    String newStockName = scanner.nextLine();
+                    Stock newStock = new Stock(newStockName);
+                    stocks.add(newStock);
+                    System.out.println("Estoque cadastrado com sucesso.");
+                    break;
+                case 2:
+                    Product productToAdd = new Product("name1", "seção1", "tipo1", 1, "marca1");
+                    stocks.get(0).addProduct(productToAdd);
+                    break;
+                case 3:
+                    Product product = stocks.get(0).findProductByIdentificationNumber(1);
+                    System.out.println(product);
+                    break;
+                case 4:
+                    Product productToUpdate = stocks.get(0).findProductByIdentificationNumber(1);
+                    productToUpdate.updateProduct();
+                    break;
+                case 5:
+                    stocks.get(0).deleteProduct(1);
+                    break;
+                case 6:
+                    try {
+                        stocks.get(0).listProducts();
+                    } catch (Exception exception) {
+                        System.out.println("Ocorreu um erro. Certifique-se de ter cadastrado um estoque.");
+                    }
+                    break;
+                case 7:
+                    System.out.println("até mais");
+                    return;
+                default:
+                    throw new IllegalArgumentException("opção inválida");
+            }
+        }
     }
 }
 
@@ -33,7 +74,8 @@ class Stock {
     private int identificationNumber;
     private static int identificationNumberCounter;
 
-    Stock() {
+    Stock(String name) {
+        this.name = name;
         this.identificationNumber = identificationNumberCounter;
 
         identificationNumberCounter++;
@@ -51,8 +93,19 @@ class Stock {
         // ?
     }
 
-    void addProduct(Product product) {
-        this.products.add(product);
+    void addProduct(Product newProduct) {
+        for (Product product : this.products) {
+            boolean haveSameNameAndBrand = newProduct.getName().equals(product.getName()) &&
+                                           newProduct.getBrand().equals(product.getBrand());
+
+            if (haveSameNameAndBrand) {
+                product.setQuantityInStock(product.getQuantityInStock() + newProduct.getQuantityInStock());
+
+                return;
+            }
+        }
+
+        this.products.add(newProduct);
     }
 
     Product findProductByIdentificationNumber(int productIdentificationNumber) {
@@ -94,15 +147,36 @@ class Product {
     private String name;
     private String department;
     private String type;
-    private int quantityInStock;
+    private int quantityInStock; // maybe double for things given in kg for example?
     private String brand;
     private int identificationNumber;
     private static int identificationNumberCounter;
 
-    Product() {
+    Product(String name, String department, String type, int quantityInStock, String brand) {
+        this.name = name;
+        this.department = department;
+        this.type = type;
+        this.quantityInStock = quantityInStock;
+        this.brand = brand;
         this.identificationNumber = identificationNumberCounter;
 
         identificationNumberCounter++;
+    }
+
+    String getName() {
+        return this.name;
+    }
+
+    int getQuantityInStock() {
+        return this.quantityInStock;
+    }
+
+    void setQuantityInStock(int quantity) {
+        this.quantityInStock = quantity;
+    }
+
+    String getBrand() {
+        return this.brand;
     }
 
     int getIdentificationNumber() {
